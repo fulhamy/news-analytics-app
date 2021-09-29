963dc6d5a06e851be39f26cbc4872699062f91b8
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-09-28 22:49:24 -0700

    Update python.py

--- python.py
+++ python.py
@@ -1,5 +1,13 @@
 import yfinance as yf
 import streamlit as st
+import psycopg2, os
+
+# read database connection url from the enivron variable we just set.
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+read_table = "SELECT date_Trunc('month', "published_at"::date), count(distinct "UID"), count("UID") from news_log where length("published_at") > 10 group by 1 having count("UID") > 100 order by 1"
+
+con = None
 
 st.write("""
 # Simple Stock Price App
@@ -23,3 +31,28 @@ st.write("""
 ## Volume Price
 """)
 st.line_chart(tickerDf.Volume)
+
+try:
+    # create a new database connection by calling the connect() function
+    con = psycopg2.connect(DATABASE_URL)
+
+    #  create a new cursor
+    cur = conn.cursor()
+    cur.execute(read_table)
+
+    print(cur.fetchall())
+       
+     # close the communication with the HerokuPostgres
+    cur.close()
+except Exception as error:
+    print('Could not connect to the Database.')
+    print('Cause: {}'.format(error))
+
+finally:
+    # close the communication with the database server by calling the close()
+    if con is not None:
+        con.close()
+        print('Database connection closed.')
+
+
+
