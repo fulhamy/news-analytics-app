f530d43aeff9214257beaafd4660b7aaf3169e1c
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-10-13 23:59:49 -0700

    Update python.py

--- python.py
+++ python.py
@@ -30,14 +30,6 @@ tickerData = yf.Ticker(tickerSymbol)
 tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
 # Open	High	Low	Close	Volume	Dividends	Stock Splits
 
-st.write("""
-## Closing Price
-""")
-st.line_chart(tickerDf.Close)
-st.write("""
-## Volume Price
-""")
-st.line_chart(tickerDf.Volume)
 
 try:
     # create a new database connection by calling the connect() function
