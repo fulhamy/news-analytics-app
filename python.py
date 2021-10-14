5c41cc7ca32cdaf5b61b3473e163c5573e8c09ed
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-10-14 00:07:32 -0700

    Update python.py

--- python.py
+++ python.py
@@ -21,16 +21,6 @@ st.write("""
 The public consciousness is shaped by the News. The purpose of this project is to shine a light on how Australia's public broadcaster has directed the public square in the digital age.
 """)
 
-# https://towardsdatascience.com/how-to-get-stock-data-using-python-c0de1df17e75
-#define the ticker symbol
-tickerSymbol = 'GOOGL'
-#get data on this ticker
-tickerData = yf.Ticker(tickerSymbol)
-#get the historical prices for this ticker
-tickerDf = tickerData.history(period='1d', start='2010-5-31', end='2020-5-31')
-# Open	High	Low	Close	Volume	Dividends	Stock Splits
-
-
 try:
     # create a new database connection by calling the connect() function
     con = psycopg2.connect(DATABASE_URL)
