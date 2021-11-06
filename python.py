1e1a7596b7d2075ea1d7c17989b554b5bf05706c
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-11-05 23:45:00 -0700

    Update python.py

--- python.py
+++ python.py
@@ -27,7 +27,7 @@ try:
 
     #  create a new cursor
     cur = con.cursor()
-    read_table = """SELECT year(date) as date, sum(articles) as articles from mymatview2 group by 1 order by 1"""
+    read_table = """SELECT date_trunc('year', date) as date, sum(articles) as articles from mymatview2 group by 1 order by 1"""
     cur.execute(read_table)
     df = pd.read_sql_query(read_table, con)
     df = df.set_index('date')
