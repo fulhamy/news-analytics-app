41ec963b27e12797a49f4a281ff5a3c4b2551df7
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-12-11 00:16:54 -0800

    Update python.py

--- python.py
+++ python.py
@@ -59,17 +59,17 @@ st.plotly_chart(fig, use_container_width=True)
  
 col1, col2 = st.columns(2)
 
-Col1.header('Polarity')
+col1.header('Polarity')
 
 fig1 = px.line(px_data, x='date', y=px_data.polarity.round(4),text=px_data.polarity.round(4))
-Col1.st.plotly_chart(fig1, use_container_width=True)
+col1.st.plotly_chart(fig1, use_container_width=True)
 
 
-Col2.st.write("""
+col2.st.write("""
 ## Subjectivity
 """)
 fig2 = px.line(px_data, x='date', y=px_data.subjectivity.round(4),text=px_data.subjectivity.round(4))
-Col2.st.plotly_chart(fig2, use_container_width=True)
+col2.st.plotly_chart(fig2, use_container_width=True)
 
 
 
