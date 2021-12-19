b3d233d57fb1bf6783a460755d2a34c22d29bf71
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-12-19 13:07:53 -0800

    Update python.py

--- python.py
+++ python.py
@@ -17,6 +17,8 @@ hide_streamlit_style = """
             """
 st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
 
+st.set_page_config(layout="wide")
+
 st.write("""
 # ABC News 
 Public consciousness is shaped by the News. The purpose of this project is to shine a light on how Australia's public broadcaster influences the public square in the digital age.
@@ -55,7 +57,7 @@ st.write("""
 ## Articles by Year
 """)
 fig = px.bar(px_data, x='date', y=px_data.articles.round(2),text='articles')
-st.plotly_chart(fig, use_container_width=FALSE)
+st.plotly_chart(fig, use_container_width=true)
  
 col1, col2 = st.columns(2)
 
@@ -63,10 +65,10 @@ st.write("""
 ## Polarity
 """)
 fig1 = px.line(px_data, x='date', y=px_data.polarity.round(4),text=px_data.polarity.round(4))
-st.plotly_chart(fig1, use_container_width=FALSE)
+st.plotly_chart(fig1, use_container_width=true)
 
 st.write("""
 ## Subjectivity
 """)
 fig2 = px.line(px_data, x='date', y=px_data.subjectivity.round(4),text=px_data.subjectivity.round(4))
-st.plotly_chart(fig2, use_container_width=FALSE)
+st.plotly_chart(fig2, use_container_width=true)
