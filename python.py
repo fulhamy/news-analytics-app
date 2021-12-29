6377bddb315af6493d8cd265f1cacb6d9a850e2a
David Fulham <42609423+fulhamy@users.noreply.github.com>
2021-12-28 21:44:50 -0800

    Update python.py

--- python.py
+++ python.py
@@ -60,8 +60,7 @@ px_data['articles'] = convert_to_thousands(px_data.articles.astype(int))
 st.write("""
 ## Articles by Year
 """)
-fig = px.bar(px_data, x='date', y='articles',text='articles',title="Count of Articles by Year")
-fig.update_traces(textfont_size=12, textangle=0, textposition="outside")
+fig = px.bar(px_data, x='date', y='articles',text='articles',title="Count of Articles by Year",textposition="outside")
 st.plotly_chart(fig, use_container_width=True)
  
 col1, col2 = st.columns(2) 
