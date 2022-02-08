00b5324390ede65bf8c4234b1e1f5e3de5b89e44
David Fulham <42609423+fulhamy@users.noreply.github.com>
2022-02-07 23:24:29 -0800

    Update python.py

--- python.py
+++ python.py
@@ -3,6 +3,9 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import python # import your app modules here
 
+from PIL import Image
+
+icon = Image.open("news_icon.png")
 
 st.set_page_config(layout="wide",page_title='Paper Tale',page_icon=icon)
 
