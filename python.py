f6fc2bebf19185de0577c10714b77cb39986b285
David Fulham <42609423+fulhamy@users.noreply.github.com>
2022-02-07 23:21:27 -0800

    Update python.py

--- python.py
+++ python.py
@@ -3,6 +3,9 @@ import streamlit as st
 from multiapp import MultiApp
 from apps import python # import your app modules here
 
+
+st.set_page_config(layout="wide",page_title='Paper Tale',page_icon=icon)
+
 app = MultiApp()
 
 # Add all your application here
