f6baf3998d76fd3913861485f67081867b2b372f
David Fulham <42609423+fulhamy@users.noreply.github.com>
2022-02-07 23:27:19 -0800

    Update python.py

--- python.py
+++ python.py
@@ -9,6 +9,15 @@ icon = Image.open("news_icon.png")
 
 st.set_page_config(layout="wide",page_title='Paper Tale',page_icon=icon)
 
+
+hide_streamlit_style = """
+                        <style>
+                        #MainMenu {visibility: hidden;}
+                        footer {visibility: hidden;}
+                        </style>
+                        """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+
 app = MultiApp()
 
 # Add all your application here
